bc5169887f0be19e1042650f12c564f296cd8da8
Patrice Cadiot <pathibul.r@gmail.com>
2018-02-09 11:54:40 +0100

    crée la vue/contrôleur User pour afficher tous les champs du profil d'un utilisateur

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname, :avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :avatar, :phone_number, :description])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_02_09_092542) do
+ActiveRecord::Schema.define(version: 2018_02_09_101705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2018_02_09_092542) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string "phone_number"
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
