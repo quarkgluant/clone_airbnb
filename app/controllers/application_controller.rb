5d65ede19eab453c40189282b08990ac0c9722d1
Patrice Cadiot <pathibul.r@gmail.com>
2018-02-18 13:20:28 +0100

    modifie la page de profil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname, :avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :avatar, :phone_number, :description])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :avatar, :phone_number, :description, :email, :password, :password_confirmation])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', edit: 'profile'}
+  devise_for :users,
+             path: '',
+             path_names:
+                 {
+                   sign_in: 'login',
+                   sign_out: 'logout',
+                   edit: 'profile'
+                 },
+             controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   resources :users, only: :show
