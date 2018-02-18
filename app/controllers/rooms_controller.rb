630d80bdc89dada97abaffc8abfa3a7555b81c5f
Patrice Cadiot <pathibul.r@gmail.com>
2018-02-18 16:16:31 +0100

    crée le contrôleur Room

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+# Room controller
+class RoomsController < ApplicationController
+  # Room Controller
+  before_action :set_room, only: [:show, :edit, :update]
+  before_action authenticate_user!, except: :show
+
+  def index
+    @rooms = current_user.rooms
+  end
+
+  def new
+    @room = current_user.rooms.build
+  end
+
+  def create
+    @room = current_user.rooms.build(room_params)
+    if @room.save
+      redirect_to @room, notice: 'Votre annonce a bien été créée'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @room.update(room_params)
+      redirect_to @room, notice: 'Votre modification a bien été enregistrée'
+    else
+      render :neditew
+    end
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:home_type, :room_type, :accommodate, :bed_room, :bath_room,
+                                 :listing_name, :summary, :address, :is_wifi, :is_tv, :is_closet,
+                                 :is_shampoo, :is_breakfast, :is_heating, :is_air, :is_kitchen, :price,
+                                 :active)
+  end
+end
