72287a06493b55ca8a95122bb867526994cfae56
Patrice Cadiot <pathibul.r@gmail.com>
2018-02-18 19:54:59 +0100

    crée le modèle Photo et son intégration avec Paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :fullname, presence: true, length: {maximum: 50}
+  validates :fullname, presence: true, length: { maximum: 50 }
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/default_image.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  has_attached_file :avatar,
+                    styles: { medium: '300x300>', thumb: '100x100>' },
+                    default_url: '/assets/default_image.png'
+  validates_attachment_content_type :avatar, content_type: %r{\Aimage\/.*\z}
 
   has_many :rooms
 end
