bb8366da1b6d9899e09ffba76bb2bfd96555aa7a
Patrice Cadiot <pathibul.r@gmail.com>
2018-03-11 14:00:30 +0100

    crée le contrôleur Reservation

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,14 @@
+class ReservationsController  < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @reservation = current_user.reservations.create(reservation_params)
+    redirect_to @reservation.room, notice: 'Votre réservation a bien été enregistrée'
+  end
+
+   private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date, :total, :price, :room_id)
+  end
+end
