2c51fec59a53094fe6808c9013547e2d44ab68b2
Patrice Cadiot <pathibul.r@gmail.com>
2019-01-29 15:22:44 +0100

    modify the ruby version to 2.6.0

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# need for load of the .env by the dotenv gem
+Dotenv::Railtie.load
+
 module CloneAirbnb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
